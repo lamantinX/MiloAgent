10bcca8df19d5213abd58fe628e2664d98dc5fd7
lamantinX <electroviolence.to.you@gmail.com>
2026-07-17 02:07:53 +0700

    Clean up agent cruft files

--- run_manual.py
+++
@@ -1,8 +0,0 @@
-import traceback
-try:
-    from tests.test_reddit_oauth import test_oauth_start_endpoint_valid
-    class DummyPatch:
-        def setattr(self, a, b): pass
-    test_oauth_start_endpoint_valid(DummyPatch())
-except Exception as e:
-    traceback.print_exc()
